2b87254734977134afed06c19053e04177b44b99
Sumit Kumar <sumitsuthar103@gmail.com>
2026-05-19 11:34:15 +0530

    Time: 3 ms (87.91%) | Memory: 43.4 MB (40.37%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }
+            else {
+                if (st.isEmpty()) {
+                    return false;
+                }
+                if ((ch == ')' && st.peek() == '(') ||
+                    (ch == '}' && st.peek() == '{') ||
+                    (ch == ']' && st.peek() == '[')) {
+                    st.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
